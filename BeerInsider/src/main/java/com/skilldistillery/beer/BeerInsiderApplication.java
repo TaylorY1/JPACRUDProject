fef08553f5754b0ef53d3334a89351754cc777f9
TaylorY1 <YockeyT139@gmail.com>
2022-04-15 19:30:47 -0600

    Update

---
+++ BeerInsider/src/main/java/com/skilldistillery/beer/BeerInsiderApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerInsiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerInsiderApplication.class, args);
+	}
+
+}
---
+++ BeerInsider/src/test/java/com/skilldistillery/beer/BeerInsiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInsiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
